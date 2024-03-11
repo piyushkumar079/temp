79484f4108e2eddce4e659c72161ed826139b070
Piyush Kumar <piyushkumar07999@gmail.com>
2024-03-11 15:45:12 +0530

    some some

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from "express";
-
+import dotenv from "dotenv"
+dotenv.config();
 const app=express();
 
 
+app.use((req,res,next)=>{
+    console.log(req.url,req.method);
+    next();
+})
+
 app.get("/",(req,res)=>{
-    res.send(hello);
+    res.send("hello");
 })
 
 app.listen(process.env.PORT,()=>{
-    console.log("Server Started");
+    console.log("Server Started at",process.env.PORT);
 })
